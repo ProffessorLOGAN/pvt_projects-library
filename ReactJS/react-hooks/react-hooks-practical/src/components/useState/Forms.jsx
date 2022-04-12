7c31624f4307d5f3d2560308cbef938f3936bdee
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-04-12 20:16:09 +0530

    Forms state using useStateHooks
    Date-12/4/22

--- ReactJS/react-hooks/react-hooks-practical/src/components/useState/Forms.jsx
+++ ReactJS/react-hooks/react-hooks-practical/src/components/useState/Forms.jsx
@@ -2,28 +2,49 @@ import React, { useState } from "react";
 
 function Forms() {
   const [name, setName] = useState();
+  const [name2, setName2] = useState();
   const [string, setstringName] = useState();
+  const [string2, setstringName2] = useState();
 
   const inputEvent = (event) => {
     setName(event.target.value);
   };
-  const onSubmit = () => {
+
+  const inputEvent2 = (event) => {
+    setName2(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
     setstringName(name);
+    setstringName2(name2);
   };
 
   return (
     <>
       <div className="content">
-        <h1>{string}</h1>
-        <input
-          type="text"
-          placeholder="Enter any String"
-          onChange={inputEvent}
-          defaultvalue=""
-        />
-        <button className="btn" onClick={onSubmit}>
-          click me
-        </button>
+        <form onSubmit={onSubmit}>
+          <h1>
+           
+            Hello {string} {string2}
+          </h1>
+
+          <input
+            type="text"
+            placeholder="Enter your firstname"
+            onChange={inputEvent}
+            defaultvalue=""
+          />
+          <input
+            type="text"
+            placeholder="Enter your lastname"
+            onChange={inputEvent2}
+            defaultvalue=""
+          />
+          <button type="submit" className="btn">
+            click me
+          </button>
+        </form>
       </div>
     </>
   );
